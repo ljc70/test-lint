fe5ba3e15afb0096316cace137fcc85ad7bcf42f
ljc <704838166@qq.com>
2020-03-13 15:29:27 +0800

    test

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,6 +3,13 @@ const micromatch = require('micromatch')
 module.exports = {
   '*': allFiles => {
     let reg = /^(\.).*$/
+    // allFiles = allFiles.includes('/') ? allFiles.split('/')
+    if (allFiles.includes('/')) {
+      allFiles.map(item => {
+        let arr = item.split('/')
+        return arr[arr.length - 1]
+      })
+    } 
     console.log('match----->111', allFiles)
     const match = micromatch(allFiles, ['*.js'])
     console.log('match----->', match)
